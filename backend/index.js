bc697cbe2be5f86e2d442b5535e8368dde81a52c
Shubham Mahobia <mahobiashubham4@gmail.com>
2024-08-05 21:20:16 +0530

    login and signup created

--- backend/index.js
+++ backend/index.js
@@ -2,14 +2,19 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
+const cors = require("cors");
+const userApi = require("./routes/user");
 var app = express();
 const port = 8080;
 app.listen(port, function () {
   console.log(`Server Running on PORT ${port}`);
 });
 app.use(express.json());
+app.use(cors());
 //Connection with Database - MongoDB
 mongoose
   .connect(`${process.env.MONGO_URL}`)
   .then(() => console.log("Database connected"))
   .catch((err) => console.log("Mongo Connection Error " + err));
+
+app.use("/api", userApi);
---
+++ backend/routes/user.js
@@ -0,0 +1,81 @@
+const router = require('express').Router();
+const user = require('../models/user');
+const User = require('../models/user');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+
+//Sign Up - Proccess
+router.post('/signup', async (req, res) => {
+    try {
+     const { userName, email, password } = req.body;
+    //Here we are checking if the user has entered all the fields
+    if(!userName || !email || !password){
+        return res.status(400).json({error: "Please fill all the fields"});
+    }
+    //Here we are checking if the user already exists
+    const existingUser = await User.findOne({userName: userName});
+    if(existingUser){
+        return res.status(400).json({success: false,error: "Username already exists"});
+    }
+    //Here we are checking if the Username/password is atleast 6 characters long
+    if(password.length < 6 || userName.length < 4){
+        return res.status(400).json({success: false,error: "Username/Password must be atleast 6 characters long"});
+    }
+    //Here we are checking if the email is valid
+    const existingEmail = await User.findOne({email: email});
+    if(existingUser){
+        return res.status(400).json({success: false,error: "Email already exists"});
+    }
+    //Hashing the password
+    const salt = await bcrypt.genSalt(12);
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    //Creating a new user
+    const newUser = new User({
+        userName,
+        email,
+        password: passwordHash
+    });
+    await newUser.save();
+
+
+    return res.status(200).json({success: true, message: "User created successfully"});
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({success: false,error: "Internal Server Error"});
+    }
+});
+
+//Login - Proccess
+router.post('/login', async (req, res) => {
+    try {
+        const { userName, password } = req.body;
+        //Here we are checking if the user has entered all the fields
+        if(!userName || !password){
+            return res.status(400).json({error: "Please fill all the fields"});
+        }
+        //Here we are checking if the user exists
+        const existingUser = await User.findOne({userName: userName});
+        if(!existingUser){
+            return res.status(400).json({success: false,error: "Invalid Username/Password"});
+        }
+        //Here we are checking if the password is correct
+        bcrypt.compare(password, existingUser.password, (err, isMatch) => {
+            if(err){
+                return res.status(400).json({success: false,error: "Invalid Username/Password"});
+            }
+            if(!isMatch){
+                return res.status(400).json({success: false,error: "Invalid Username/Password"});
+            }
+            const token = jwt.sign({id: existingUser._id}, 'ndoiawnuonwuonq', {expiresIn: "3d"});
+            return res.status(200).json({success: true, message: "User logged in successfully", token: token});
+        });
+        
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({success: false,error: "Internal Server Error"});
+    }
+});
+
+module.exports = router;
